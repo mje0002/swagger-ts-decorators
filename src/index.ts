414dd9efcbfb6682eede9d29ed7cc30db8534ded
mje0002 <mje0002@gmail.com>
2023-01-29 21:47:45 -0500

    Package changes

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,29 @@ export { IApiPathArgs, ApiPath } from '@/decorators/api-path.decorator';
 export {
   IApiOperationGetArgs,
   ApiOperationGet,
-} from '@/api-operation-get.decorator';
+} from '@/decorators/api-operation-get.decorator';
 export {
   IApiOperationPostArgs,
   ApiOperationPost,
-} from '@/api-operation-post.decorator';
+} from '@/decorators/api-operation-post.decorator';
 export {
   IApiOperationPutArgs,
   ApiOperationPut,
-} from '@/api-operation-put.decorator';
+} from '@/decorators/api-operation-put.decorator';
 export {
   IApiOperationPatchArgs,
   ApiOperationPatch,
-} from '@/api-operation-patch.decorator';
+} from '@/decorators/api-operation-patch.decorator';
 export {
   IApiOperationDeleteArgs,
   ApiOperationDelete,
-} from '@/api-operation-delete.decorator';
+} from '@/decorators/api-operation-delete.decorator';
 
 export {
   IApiModelPropertyArgs,
   ApiModelProperty,
-} from '@/api-model-property.decorator';
-export { IApiModelArgs, ApiModel } from '@/api-model.decorator';
+} from '@/decorators/api-model-property.decorator';
+export { IApiModelArgs, ApiModel } from '@/decorators/api-model.decorator';
 
 export { SwaggerDefinitionConstant } from '@/swagger-definition.constant';
 export { express, ISwaggerExpressOptions } from '@/express.configurator';
